a456e825ebf5adae81890982e36429717dc7f346
ilton <xlsonplay@gmail.com>
2022-12-17 08:02:59 -0300

    add required input

--- app/script.js
+++ app/script.js
@@ -1,27 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Document</title>
-	</head>
-	<body>
-	
-		<!--Imprimir variavel -->
-
-		
-		<ul>
-			{% for pessoa in pessoas %}
-				<li> {{ pessoa.id }} - {{ pessoa.nome }}
-					<a href="{% url 'editar' pessoa.id %}">Editar</a>
-					<a href="{% url 'delete' pessoa.id %}">Deletar</a>
-				
-				</li>
-			{% endfor %}
-    	</ul>
-		
-<script>
     function checkForm() {
         // Fetching values from all input fields and storing them in variables.
         var nome = document.getElementById("nome").value;
@@ -45,17 +21,3 @@
 
         }
     }
-</script>
-		
-		
-		<form action="{% url 'salvar' %}" method="POST" onsubmit="return checkForm()">
-			{% csrf_token %}
-			<input type="text" name="nome" placeholder="Nome" id="nome" required>
-			<button type="submit" value="[ Enviar Mensagem ]">Salvar</button>
-		</form>
-		
-
-	
-	
-	</body>
-</html>
